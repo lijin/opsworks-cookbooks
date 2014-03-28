5359f0ed8a1538fe8fa13dd5c6e39117f2d1002d
Li <contact@lijin.name>
2014-03-28 14:30:13 +1100

    Increase the size of swapfile to 2048M and enable memory overcommit

--- swap/recipes/setup.rb
+++ swap/recipes/setup.rb
@@ -9,10 +9,14 @@ script "create_swap" do
   interpreter "bash"
   user "root"
   code <<-EOH
-  sudo dd if=/dev/zero of=#{swapfile} bs=1M count=1024
+  sudo dd if=/dev/zero of=#{swapfile} bs=1M count=2048
   sudo mkswap #{swapfile}
   sudo swapon #{swapfile}
   echo "#{swapfile} swap swap defaults 0 0" >> /etc/fstab
   EOH
   not_if { ::File.exists?(swapfile) }
+end
+
+execute "enable_overcommit_memory" do
+  command "sudo sysctl vm.overcommit_memory=1"
 end
